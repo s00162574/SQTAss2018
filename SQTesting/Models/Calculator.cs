84c9cd47296c458a7313afa99e705391aca490d7
s00162574 <s00162574@LAB-D2037-35>
2018-03-05 10:25:36 +0000

    Initial commit

---
+++ SQTesting/Models/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SQTesting.Models
+{
+    public class Calculator
+    {
+        public float CalcPremium(int age, string gender)
+        {
+            float premium;
+
+            if (gender == "female")
+                if ((age >= 18) && (age <= 30))
+                    premium = 5.0f;
+                else
+                    if (age >= 31)
+                    premium = 2.50f;
+                else
+                    premium = 0.0f;
+            else
+    if (gender == "male")
+                if ((age >= 18) && (age <= 35))
+                    premium = 6.0f;
+                else
+                   if (age >= 36)
+                    premium = 5.0f;
+                else
+                    premium = 0.0f;
+            else
+                premium = 0.0f;
+
+            if (age >= 50)
+                premium = premium * 0.15f;
+            return premium;
+        }
+
+    }
+}
